d2f004b1ddf83d3f6042f60a83f8b75ac9d06c9e
Lalex <alejandro.gamero@hotmail.com>
2018-08-20 21:05:21 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Website Project Starter Kit 2018
+
+This repo is a local environment that you can use to optimize, minify your CSS, JS, & HTML files in order to improve your project performance. This is how I start my projects, maybe you can use it too :)
+
+## Requirements
+In order to use this project you will need some requirements:
+1. Install [NodeJS](https://nodejs.org/en/): that will help you to run Javascript on the console.
+2. Install Gulp with the following command: ```npm install -g gulp```
+
+Gulp is the one that will run all the compilation, watchers and others tasks. To learn more about Gulp and all it's magical abilities check out there website [here](https://gulpjs.com/).
+
+## Download / Clone
+
+Please feel free to clone or download the source files into your computer:
+
+```url```
+
+Alternatively, you can [download](url) this repository.
+
+## Install required modules
+Open your terminal window and change into the source directory where you have cloned or downloaded the repo to. Run the command:
+
+```npm install```
+
+## Initialise the environment
+Once all of the required node modules have been downloaded and installed, run the following command:
+
+```gulp```
+
+This will generate the CSS, JS, & HTML files. It will compile the project and start a server that will refresh every time you change something in the code.
+
+Here are commands you can run:
+* __default__: Clean the output directory and then compile CSS, JS, & HTML files and watch for changes.
+* __scripts__: Concatenate all JS file into a single file, minify the single JS file.
+* __cssnano__: Compile Sass files, minify and optimize CSS files, add auto-prefixers.
+* __pages__: Compile and minify HTML files
+* __watch__: Watch for any changes on each section.
+
+## Folder Structure
+
+|-- dist
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|-- css
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|-- style.min.css
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|-- js
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|-- main.min.js
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|-- index.html (minified)
+|-- src
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|-- css
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|-- main.css
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|-- js
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|-- sass
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|-- index.html
+
+This is just a basic start but work for most basic project. Enjoy!
+Happy coding!
---
+++ gulpfile.js
@@ -0,0 +1,116 @@
+'use strict';
+
+// Define some plugins
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create(); // create a browser sync instance
+var cssnano = require('gulp-cssnano');
+var concat = require('gulp-concat');
+var del = require('del');
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var sourceMaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+
+
+// Serve files from the root of this project
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+        baseDir: "./dist"
+    },
+    notify: false
+  });
+});
+
+
+// Compile Sass files
+gulp.task('postcss', function() {
+  return gulp.src('./src/sass/**/*.sass')
+    .pipe(sourceMaps.init())
+    .pipe(sass({
+       errLogToConsole: true,
+       outputStyle: 'expanded'
+    }))
+    .pipe(postcss([
+       autoprefixer({ browsers: ['last 4 version'] })
+    ]))
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest('./src/css/'))
+    .pipe(browserSync.stream())
+});
+
+
+// Minify and optimize style.css.
+gulp.task('cssnano', ['postcss'], function() {
+  return gulp.src('./src/css/main.css')
+   // Optimize and minify main.css
+   .pipe(cssnano({
+      safe: true // Use safe optimizations
+   }))
+   // Rename main.css to style.min.css
+   .pipe(rename('style.min.css'))
+   // Write style.min.css to the dist directory
+   .pipe(gulp.dest('./dist/css/'))
+   .pipe(browserSync.stream());
+});
+
+
+// Gulp task to minify JavaScript files
+gulp.task('scripts', function() {
+  return gulp.src('./src/js/**/*.js')
+    // Concatenate all JS file into single file
+    .pipe(concat('main.js'))
+    // Minify the file
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    // Output
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.reload({
+      stream: true  // prompts a reload after compilation
+    }))
+});
+
+
+// Gulp task to minify HTML files
+gulp.task('pages', function() {
+  return gulp.src(['./src/**/*.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.reload({
+      stream: true // prompts a reload after compilation
+    }))
+});
+
+
+// Watch for changes within the source folder
+gulp.task('watch', function() {
+    // Watch Sass, JS & HTML files
+    gulp.watch('./src/sass/**/*.sass', ['cssnano']);
+    gulp.watch('./src/js/**/*.js', ['scripts']);
+    gulp.watch('./src/**/*.html', ['pages']);
+    // gulp.watch(['src/*.html', 'src/sass/**/*.scss', 'src/js/**/*.js']).on('change', browserSync.reload);
+});
+
+
+// Clean output directory
+gulp.task('clean', () => del(['dist']));
+
+
+// Gulp task to minify all files
+gulp.task('default', ['clean'], function () {
+  runSequence(
+    'browserSync',
+    'cssnano',
+    'scripts',
+    'pages',
+    'watch'
+  );
+});
